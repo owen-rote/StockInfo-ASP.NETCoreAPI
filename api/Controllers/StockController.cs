9338752fcf6660663ec71338b46feaab6fd1b7af
owen-rote <rotenbergowen@gmail.com>
2025-02-21 20:13:37 -0800

    add DTOs

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -50,5 +50,46 @@ namespace api.Controllers
 
       return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDto());
     }
+
+    [HttpPut]
+    [Route("{id}")]
+    public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateDto)
+    {
+      var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+      if (stockModel == null)
+      {
+        return NotFound();
+      }
+
+      stockModel.Symbol = updateDto.Symbol;
+      stockModel.CompanyName = updateDto.CompanyName;
+      stockModel.Purchase = updateDto.Purchase;
+      stockModel.LastDiv = updateDto.LastDiv;
+      stockModel.Industry = updateDto.Industry;
+      stockModel.MarketCap = updateDto.MarketCap;
+
+      _context.SaveChanges();
+
+      return Ok(stockModel.ToStockDto());
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+      var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+      if (stockModel == null)
+      {
+        return NotFound();
+      }
+
+      _context.Stocks.Remove(stockModel);
+
+      _context.SaveChanges();
+
+      return NoContent();
+    }
   }
 }
