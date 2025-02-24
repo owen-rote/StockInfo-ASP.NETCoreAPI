fd2e545f7c167830d4fa2b7355516217add5a2de
owen-rote <rotenbergowen@gmail.com>
2025-02-24 11:37:58 -0800

    add token service

--- api/Program.cs
+++ api/Program.cs
@@ -2,6 +2,7 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repository;
+using api.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.CodeAnalysis.Options;
@@ -59,6 +60,7 @@ builder.Services.AddAuthentication(options =>
 // Interfaces
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 
 // Newtonsoft
